ef9b900530d7f5604a241d24a839527500e5ba75
pretentiousUsername <mitchell.ian.2001@gmail.com>
2024-12-14 12:31:18 -0500

    basic roadmap

--- src/Symian.jl
+++ src/Symian.jl
@@ -1,4 +1,10 @@
 module Symian
+using SymbolicUtils
+using Symbolics
+
+using Symbolics: isarraysymbolic, array_term
+
+include("integral.jl")
 
 greet() = print("Hello World!")
 
---
+++ src/integral.jl
@@ -0,0 +1,29 @@
+# just for now
+using SymbolicUtils
+using Symbolics
+
+using Symbolics: isarraysymbolic, array_term
+
+
+
+#associativity = @rule 
+
+
+#=
+## this is stolen from the Symbolics source code, `diff.jl`, specifically
+abstract type Operator end
+propagate_shape(::Operator, x) = axes(x)
+
+struct Integral <: Operator
+
+end
+
+function (I::Integral)(x)
+    x = unwrap(x)
+    if isarraysymbolic(x)
+        array_term(D, x)
+    else
+        term(D, x)
+    end
+end
+=#
